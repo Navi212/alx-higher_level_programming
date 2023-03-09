dad1ba4138b9fea5309d0852b764397b950ad212
Nweke Joseph <josephchristo615@gmail.com>
2023-03-09 03:29:41 +0000

    Updates task 5 file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i <= 9:
-        print(0, end="")
-    print("{}".format(i), end=", " if i < 98 else ', {}\n'.format(99))
+for i in range(0, 100):
+    if i < 10:
+        print("{:02d}".format(i), end=", ")
+    elif i < 99:
+        print("{}".format(i), end=", ")
+    else:
+        print(i)
