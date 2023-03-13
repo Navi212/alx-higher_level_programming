f77023a50dd59f17e4245faa07c709a74a2a1b1d
Nweke Joseph <josephchristo615@gmail.com>
2023-03-13 22:20:13 +0000

    Adds return value

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    """Retrives an element from a list"""
-    list_len = len(my_list)
-    if idx < 1:
-        return None
-    if idx > list_len:
-        return None
-    for i in my_list:
-        val = my_list[idx]
-        return val
-
-if __name__ == "__main__":
-    element_at(0, 0)
+    if (idx < 0 or idx > len(my_list) - 1):
+        return (None)
+    return (my_list[idx])
