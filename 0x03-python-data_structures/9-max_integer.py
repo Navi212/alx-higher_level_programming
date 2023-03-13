e17a5c4cf470b6b1d4193d8f055a93a4ddfa788d
Nweke Joseph <josephchristo615@gmail.com>
2023-03-13 23:00:31 +0000

    Updates task 9 file

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
-    """Finds the biggest integer of a list"""
-    list_len = len(my_list)
-    if len(my_list) <= 0:
-        return None
-    max_val = my_list[0]
+    if (len(my_list) == 0):
+        return(None)
+    max = my_list[0]
     for i in my_list:
-        if my_list[i] > max_val:
-            max_val = my_list[i]
-        return max_val
+        if (i > max):
+            max = i
+    return (max)
