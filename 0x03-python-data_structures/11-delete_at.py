ee07da318759dff679926981e1e200982392601f
Nweke Joseph <josephchristo615@gmail.com>
2023-03-13 23:25:05 +0000

    Removed redundancy

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,13 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     """Deletes the item at a specific position in a list"""
-
-    list_len = len(my_list)
-    if idx < 0:
-        return my_list
-    elif idx > list_len:
-        return my_list
-    else:
-        for i in my_list:
-            del my_list[idx]
-            return my_list
+    if (idx < 0 or idx > len(my_list) - 1):
+            return (my_list)
+    del my_list[idx]
+    return (my_list)
