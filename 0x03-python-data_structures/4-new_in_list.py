837a96cdd3004f52770e211d6bd640ee9705def1
Nweke Joseph <josephchristo615@gmail.com>
2023-03-13 22:48:55 +0000

    Updates file to replace list element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,13 +5,8 @@ def new_in_list(my_list, idx, element):
     Replaces an element in a list at a particular index without
     modifying the original list
     """
-
-    list_len = len(my_list)
     list_copy = my_list[:]
-    if idx < 0:
+    if (idx < 0 or idx > len(my_list) - 1):
         return list_copy
-    if idx > list_len:
-        return list_copy
-    for i in list_copy:
-        list_copy[idx] = element
-        return list_copy
+    list_copy[idx] = element
+    return list_copy
