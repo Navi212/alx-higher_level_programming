6cc92c16b7ca4a54d94aacb2465bf79bfa7ee2bd
Nweke Joseph <josephchristo615@gmail.com>
2023-03-13 22:35:34 +0000

    Updates file removing redundancy

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,13 +2,9 @@
 
 def replace_in_list(my_list, idx, element):
     """Replaces an element of a list at a specified location"""
-
     list_len = len(my_list)
-    if idx < 0:
+    if (idx < 0 or idx > len(my_list) - 1):
         return my_list
-    if idx > list_len:
-        return my_list
-    for i in my_list:
-        my_list[idx] = element
-        return my_list
+    my_list[idx] = element
+    return my_list
 
