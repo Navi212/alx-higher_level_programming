bbcc58116f5e872fb170df4ac995bb74e1fa5bb8
Nweke Joseph <josephchristo615@gmail.com>
2023-03-20 19:11:30 +0000

    Updates 1-search file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for index, item in enumerate(new_list):
-        if (item == search):
-            new_list[index] = replace
-    return new_list
+    newlist = my_list[:]
+    i = 0
+    for i in range(len(my_list)):
+        if (my_list[i] == search):
+            newlist[i] = replace
+    return (newlist)
