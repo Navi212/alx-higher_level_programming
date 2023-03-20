0ef94ee8a9f6375b136c4a17740a1ccfca5affa9
Nweke Joseph <josephchristo615@gmail.com>
2023-03-20 19:12:19 +0000

    Updates 2 uniq file

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    my_set = set()
-    if my_list:
-        for item in my_list:
-            my_set.add(item)
-    return sum(my_set)
+    result = 0
+    for i in set(my_list):
+        result += i
+    return (result)
