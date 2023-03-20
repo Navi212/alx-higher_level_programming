06b5762e74cca8d3d8ce5d7ea0b018b42d56e8c0
Nweke Joseph <josephchristo615@gmail.com>
2023-03-20 19:09:48 +0000

    Updated 0-square file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        return([list(map(lambda x:x ** 2, rows)), for row in matrix])
+    if (matrix):
+        new_matrix = []
+        for row in matrix:
+            new_matrix.append(list(map(lambda x: x * x, row)))
+        return (new_matrix)
+
