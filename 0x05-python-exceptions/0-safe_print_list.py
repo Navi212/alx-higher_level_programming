af773a04fd4c76a368d7eed62458771607215f24
Nweke Joseph <josephchristo615@gmail.com>
2023-03-29 01:14:32 +0100

    Adds all tested files

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+
+def safe_print_list(my_list=[], x=0):
+    """ Prints x elements of a list.
+
+    Args:
+    my_list - list to print from.
+    x - number of elements
+
+    Returns:
+    number of printed elements.
+    """
+    
+    num_el = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num_el += 1
+        
+        except IndexError:
+            break
+    print("")
+    return (num_el)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+
+def raise_exception_msg(message=""):
+    """ Raises a name exception with a message. """
+
+    raise NameError(message)
