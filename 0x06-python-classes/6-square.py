cc12187c6370bd70f5fe7c0e06636757a8efd644
Nweke Joseph <josephchristo615@gmail.com>
2023-03-31 00:37:38 +0100

    Updates file 6 and 8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,10 +50,12 @@ class Square:
 
     def my_print(self):
         """ Prints the square with the character # to stdout. """
-        if (self.__size == 0):
+        if self.__size == 0:
             print("")
-        else:
-            for row in range(self.__position[1]):
-                print("")
-            for row in range(0, self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
