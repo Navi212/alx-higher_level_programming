49c167af3a0fec660d02d49126faa396316ad8df
Nweke Joseph <josephchristo615@gmail.com>
2023-04-12 01:14:16 +0100

    Adds task 4 file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+The "4-inherits_from" supplies a function inherits_from()
+that checks if an Object is instance of a class that inherited
+(directly of indirectly) from a specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Returns True of obj is an instance of a_class.
+
+    Args:
+        obj - object.
+        a_class - class.
+
+    Return:
+        True if true.
+        False if false.
+    """
+    if issubclass(obj, a_class):
+        return (True)
+    return (False)
