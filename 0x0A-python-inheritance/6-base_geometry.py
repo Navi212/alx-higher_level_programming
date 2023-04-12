b8407f853f99b521c5f04acd28a8e2b48ab09436
Nweke Joseph <josephchristo615@gmail.com>
2023-04-12 01:28:23 +0100

    Adds task 6 file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+The "5-base_geometry" defines an empty class.
+"""
+
+
+class BaseGeometry:
+    """ Defines a class BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
