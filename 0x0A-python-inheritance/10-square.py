7a33c124ccca67b82582e624f0dd1d058700c683
Nweke Joseph <josephchristo615@gmail.com>
2023-04-12 03:35:30 +0100

    Adds task 10 file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+The "10-square" module defines a new Square.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines a class Square. """
+    def __init__(self, size):
+        """ Initializes Square instances.
+
+        Args:
+            size(int) - size.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Defines and computes the area. """
+        return (self.__size ** 2)
