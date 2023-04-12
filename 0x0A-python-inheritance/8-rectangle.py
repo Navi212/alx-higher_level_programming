ea081e50e5b869528487c8c4a67871ada51a8ad3
Nweke Joseph <josephchristo615@gmail.com>
2023-04-12 02:17:50 +0100

    Adds task 8 file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ The "8-rectangle" inherits from class BaseGeometry.
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectange(BaseGeometry):
+class Rectangle(BaseGeometry):
     """ Defines a class that inherits from BaseGeometry. """
     def __init__(self, width, height):
         """ Initialize a new Rectangle.
