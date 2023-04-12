c4864c7448bb43dba5dbacec79dda0414480d103
Nweke Joseph <josephchristo615@gmail.com>
2023-04-12 22:55:59 +0100

    Updates task 0 file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """
-        Reads the contents of a file
-    """
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
