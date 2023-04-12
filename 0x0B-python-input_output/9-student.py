d4f19f4a16a8677de3e7b1b967ef36a676217edc
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 00:02:25 +0100

    Updates task 9 file

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
-import sys
-save_to_json = __import__("7-save_to_json_file").save_to_json_file
-load_from_json = __import__("8-load_from_json_file").load_from_json_file
+"""class_to_json module"""
 
 
-def add_item(args, filename):
+def class_to_json(obj):
+    """dictionary description of simple data structure
+    for JSON serialization
     """
-        adds items to a json file
-        @args: The arguments that need to be added.
-        @filename: The file that needs to be updated
-    """
-    try:
-        content = load_from_json(filename)
-    except:
-        content = []
-
-    for item in args:
-        content.append(item)
-    save_to_json(content, filename)
-
-
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+    return obj.__dict__
