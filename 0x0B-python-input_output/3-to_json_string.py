4dd407af7c03e0c36c9c0d23f404fd8d7024366a
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 13:24:49 +0100

    Re-adds 3 file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""module for json string
-"""
-
-
 import json
 
 
+"""
+The ``3-to_json_string`` module supplies a function
+def to_json_string() that returns the JSON representation
+of an object.
+"""
+
+
 def to_json_string(my_obj):
-    """ json representation of object
+    """ Returns JSON representation of my_obj.
+
+    Args:
+        my_obj - The object.
+
+    Return:
+        JSON representation of my_obj.
     """
-    return json.dumps(my_obj)
+    return (json.dumps(my_obj))
