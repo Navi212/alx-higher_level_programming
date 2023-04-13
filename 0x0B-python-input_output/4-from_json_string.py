d772289a787a67018f65e66b00e51a2f53b8f02d
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 15:37:23 +0100

    Updates task 4 file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""module for json string
-"""
-
-
 import json
 
 
+"""
+The ``4-from_json_string`` module supplies a function
+``from_json_string()`` that returns an objetc represented
+by a JSON string.
+"""
+
+
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON
+    """ Returns an object represented by a JSON string.
+
+    Args:
+        my_str - string representing a JSON string.
+
+    Return:
+        JSON object.
     """
-    return json.loads(my_str)
+    return(json.loads(my_str))
