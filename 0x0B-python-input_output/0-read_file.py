58d7eaf154fffcfc29201ab47423ff50f6a531e6
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 12:35:46 +0100

    Adds updated version 0 and 1 files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""module for read_file
+"""
+The ``0-read_file`` module supplies a function
+read_file() that reads a file ``filename``.
 """
 
 
 def read_file(filename=""):
-    """read text file with utf-8
-    """
-    with open(filename, "r", encoding="utf-8") as file_tmp:
-        data_read = file_tmp.read()
-    print(data_read, end="")
+    """ Reads a text file(UTF8) and prints it to stdout. """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""module to write file
+"""
+The ``1-write_file`` module supplies a function
+write_file() that writes a string to a txt file.
 """
 
 
 def write_file(filename="", text=""):
-    """write file with text input
+    """ Writes a string to a txt file(UTF8).
+
+    Args:
+        filename - The file to write into.
+
+    Return:
+        Number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as data_file:
-        return data_file.write(text)
+    with open(filename, "w", encoding="utf-8") as file:
+        return (file.write(text))
