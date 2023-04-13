78305a2bfd1358e27c132f88e73a8bf2011d92bc
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 17:04:57 +0100

    Updates task 8 file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-The 8-class_to_json module supplies a function.
-The def class_to_json() function returns the dict-
-ionary description for JSON serialization object.
-"""
+"""The 8-class_to_json module supplies a function."""
 
 
 import json
 
 
 def class_to_json(obj):
-    """ Returns dict. description for JSON sterilation object. """
+    """ Returns dict. description for JSON object. """
     return obj.__dict__
