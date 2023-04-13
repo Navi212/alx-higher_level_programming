bd69a0136a563833aedbd9594327cfa29cbcd473
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 12:40:30 +0100

    Removes blank line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,6 @@ read_file() that reads a file ``filename``.
 
 def read_file(filename=""):
     """ Reads a text file(UTF8) and prints it to stdout. """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read())
+    with open(filename, "r" encoding="utf-8") as file:
+        file_cont = file.read()
+        print(file_cont)
