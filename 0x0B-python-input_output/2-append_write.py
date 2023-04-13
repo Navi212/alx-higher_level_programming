26b76577bea6bca24699c92840cfbb3afc4cd46a
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 13:02:03 +0100

    Removed redunancy

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
-"""appends a string at the end of a text file
+
+"""
+The ``2-append_write`` module supplies a function
+append_write() that appends a string at the end of
+a text file(UTF8) and returns the number of characters.
 """
 
 
 def append_write(filename="", text=""):
-    """appened string at end
+    """ Appends a string at the end of a text file(UTF8).
+
+    Args:
+        filename - file to append into.
+        text - string to append into filename.
+
+    Return:
+        Number of characters added.
     """
-    with open(filename, mode="a") as data_file:
-        return data_file.write(text)
+    with open(filename, "a", encoding="utf-8") as file:
+        num_chars = file.write(text)
+        return(num_chars)
