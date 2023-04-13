70301932896f15305b549ee8cbe4009d0991ec40
Nweke Joseph <josephchristo615@gmail.com>
2023-04-13 12:45:06 +0100

    Updates 0 file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """ Reads a text file(UTF8) and prints it to stdout. """
     with open(filename, "r", encoding="utf-8") as file:
         file_cont = file.read()
-        print(file_cont)
+        print(file_cont, end="")
