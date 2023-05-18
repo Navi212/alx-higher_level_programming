5098b5e3186bc2e1e8b6be491dbdb7a589f026bb
Nweke Joseph <josephchristo615@gmail.com>
2023-05-18 12:00:59 +0100

    Updates 102 file

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- ordered by temperature in descending order.
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month BETWEEN "July" AND "August"
+WHERE month BETWEEN 7 AND 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
