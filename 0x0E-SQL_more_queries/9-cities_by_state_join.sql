5fd4ed97eade9fed42c0dc9659df0695c7378cc1
Nweke Joseph <josephchristo615@gmail.com>
2023-05-19 12:16:23 +0100

    Adds 9 file

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,8 @@
--- Script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name -- Query to join cities and states
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id 
+SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id;
+JOIN states
+	ON cities.state_id = states.id
+ORDER BY cities.id;
