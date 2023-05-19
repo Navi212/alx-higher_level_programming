5035e1d8cffd298532a1f025a21d521567a2884e
Nweke Joseph <josephchristo615@gmail.com>
2023-05-19 15:57:52 +0100

    Updates 16 file

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
--- script that lists all shows, and all genres linked to that show
+-- A script that lists all shows, and all genres linked to that show
 -- from the database hbtn_0d_tvshows.
 -- If a show doesn’t have a genre, display NULL in the genre column
 -- Each record should display: tv_shows.title - tv_genres.name
