f93865b6a291c1f7b8717c2054775166f5c7465c
Nweke Joseph <josephchristo615@gmail.com>
2023-05-19 14:15:54 +0100

    Adds 15 file

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,14 @@
--- Script that lists genres of Dexter show
-SELECT tv_shows.title -- Query to join tables
-FROM tv_shows
-     JOIN tv_show_genres
-     	  ON tv_show_genres.show_id = tv_shows.id
-     JOIN tv_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
+-- A script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- The tv_genres table contains only one record where name = Comedy.
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+-- Note:Joined all tables with same key relationship(foreign keys)
+SELECT tv_shows.title
+FROM tv_genres
+JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
