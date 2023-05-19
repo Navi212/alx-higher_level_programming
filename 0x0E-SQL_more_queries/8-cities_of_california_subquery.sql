89a36607dd7d4e5951c5e3d44f99fcd2c4b45115
Nweke Joseph <josephchristo615@gmail.com>
2023-05-19 11:48:59 +0100

    Updates 8 file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,6 @@
 -- The states table contains only one record where name = California
 -- (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
-USE hbtn_0d_usa;
 SELECT id, name
 FROM cities
 WHERE state_id = (
