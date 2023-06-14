1ce76d7fc27796407fdf60b0676154c73dab5c2d
Nweke Joseph <josephchristo615@gmail.com>
2023-06-14 11:46:21 +0100

    Adds task 8 file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+if (!parseInt(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let line = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
