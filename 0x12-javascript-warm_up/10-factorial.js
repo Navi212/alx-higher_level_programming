82d5e33b2c47af81e4f1527e6dc223b8193eaf07
Nweke Joseph <josephchristo615@gmail.com>
2023-06-14 12:43:17 +0100

    Adds task 10 file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (num < 0 || num === 0 || Number.isNaN(num)) {
+    return 1;
+  } else {
+    return (factorial(num - 1) * num);
+  }
+}
+console.log(factorial(num));
