8fba6ef2c2410b52dfe57f345e0ec4f4d2c3cd2c
Nweke Joseph <josephchristo615@gmail.com>
2023-06-16 12:16:07 +0100

    Updates task 6 file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,10 +4,9 @@ const Sq = require('./5-square.js');
 
 class Square extends Sq {
   charPrint (c) {
-    if (c === undefined) {
+    if (typeof (c) === 'undefined') {
       super.print();
     } else {
-      c = 'C';
       for (let i = 0; i < this.height; i++) {
         let line = '';
         for (let j = 0; j < this.width; j++) {
