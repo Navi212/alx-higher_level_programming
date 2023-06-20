684a4d560e2ceb82fc2c344d40d7eb0c46e2b53f
Nweke Joseph <josephchristo615@gmail.com>
2023-06-20 15:48:55 +0100

    Adds 0 file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,22 +3,14 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
-
-    try:
-        conn = MySQLdb.connect(user=argv[1],
-                               passwd=argv[2], db=argv[3], port=3306)
-        cur = conn.cursor()
-        cur.execute(
-                """
-                SELECT *
-                FROM states
-                ORDER BY states.id ASC
-                """)
-        result = cur.fetchall()
-        for states in result:
-            print(states)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        cur.close()
-        conn.close()
+    conn = MySQLdb.connect(user=argv[1],
+                           passwd=argv[2], db=argv[3], port=3306)
+    cur = conn.cursor()
+    cur.execute(""" \
+            SELECT * \
+            FROM states \
+            ORDER BY states.id ASC \
+            """)
+    result = cur.fetchall()
+    for states in result:
+        print(states)
