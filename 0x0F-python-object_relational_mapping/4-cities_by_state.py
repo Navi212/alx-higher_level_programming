ba6f275062358f17ee19f725ce73c2447d5ad7f7
Nweke Joseph <josephchristo615@gmail.com>
2023-06-20 17:23:30 +0100

    Adds 4 file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+"""
+A script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     try:
-
         conn = MySQLdb.connect(user=argv[1],
                                passwd=argv[2], db=argv[3], port=3306)
         with conn.cursor() as cur:
-            cur.execute(""" \
-                    SELECT cities.id, cities.name, states.name \
-                    FROM cities \
-                    JOIN states \
-                    ON states.id = cities.state_id \
-                    ORDER BY cities.id ASC \
+            cur.execute(
+                    """
+                    SELECT cities.id, cities.name, states.name
+                    FROM cities
+                    JOIN states
+                    ON states.id = cities.state_id
+                    ORDER BY cities.id ASC
                     """)
             result = cur.fetchall()
             for cities in result:
