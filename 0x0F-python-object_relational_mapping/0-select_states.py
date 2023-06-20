47e00f67562418a48e9a63cd4ffa5fd77d8a88af
Nweke Joseph <josephchristo615@gmail.com>
2023-06-20 16:08:52 +0100

    Updates 0 file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2],
                            db=argv[3], port=3306)
     cur = conn.cursor()
