5e05752e1055a89d0c78f5c66bb3cdc09dc3b4e2
Nweke Joseph <josephchristo615@gmail.com>
2023-06-20 16:27:45 +0100

    Updates 2 file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
     try:
         conn = MySQLdb.connect(user=argv[1],
                                passwd=argv[2], db=argv[3], port=3306)
