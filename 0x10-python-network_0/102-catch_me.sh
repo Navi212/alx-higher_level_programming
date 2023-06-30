34f14e273d079fe6bd98d839524e9011d0bb41c6
Nweke Joseph <josephchristo615@gmail.com>
2023-06-30 19:09:39 +0100

    Adds 102 file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A Bash script that makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
-curl -sL -X PUT -d "You%20got%20me!" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "You%20got%20me!" 0.0.0.0:5000/catch_me
