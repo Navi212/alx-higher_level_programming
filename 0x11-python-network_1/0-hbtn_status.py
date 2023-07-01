bdfb6b4a7ca65eb980b9f7ccb19d66fd27cf56d0
Nweke Joseph <josephchristo615@gmail.com>
2023-07-02 00:05:27 +0100

    Adds 1 file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "http://localhost"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        req_response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(req_response)))
+        print("\t- content: {}".format(req_response))
+        print("\t- utf8 content: {}".format(req_response.decode("utf-8")))
