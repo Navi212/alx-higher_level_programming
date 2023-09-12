bd8f38f31ead7b76ed87a4b68404adab9f8725db
Navi212 <josephchristo615@gmail.com>
2023-09-12 15:43:45 +0100

    Updates task 9 and adds exec permission on others

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""class_to_json module"""
+"""Student class module"""
 
 
-def class_to_json(obj):
-    """dictionary description of simple data structure
-    for JSON serialization
-    """
-    return obj.__dict__
+class Student:
+    """A class that defines a Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Defines a method that retrieves a dictionary
+        representation od a Student instance.
+        """
+        return self.__dict__
