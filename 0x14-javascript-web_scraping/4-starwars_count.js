b9052862f77298e9c4deef148f0a75cf0d9c331d
Navi212 <josephchristo615@gmail.com>
2023-09-26 23:31:31 +0100

    Adds task4 file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const i in films) {
+      const chars = films[i].characters;
+      for (const j in chars) {
+        if (chars[j].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('Erorr Code:', response.statusCode);
+  }
+});
